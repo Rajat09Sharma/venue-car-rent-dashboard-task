a77e5d88d2296a484d98dbc78445f0309572a4ee
Rajat Sharma <rajat9.lyfe@gmail.com>
2025-08-31 00:42:08 +0530

    hover effect on sidebar.

--- src/components/DailyStats/PieChart/Chart.jsx
+++ src/components/DailyStats/PieChart/Chart.jsx
@@ -4,6 +4,22 @@ import { IoIosArrowRoundUp } from "react-icons/io";
 
 import PieChartImg from "../../../assets/pie-chart.png";
 
+import {
+    PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer
+} from "recharts";
+
+const pieData = [
+    { name: "Marketing", value: 200 },
+    { name: "Development", value: 300 },
+    { name: "Sales", value: 400 },
+];
+
+const COLORS = [
+    "#00c951",
+    "#fb2c36",
+    "#0088FE",
+];
+
 const DATAS = [
     {
         title: "Total Hired",
@@ -29,7 +45,27 @@ export const Chart = () => {
                 <p className="md:text-lg md:text-gray-500 md:font-semibold">Hire vs Cancel</p>
                 <p className="md:text-xs md:py-1 md:px-2 md:bg-gray-100 md:rounded-md">Today</p>
             </div>
-            <img className="md:w-35 md:mx-auto md:my-4" src={PieChartImg} />
+            {/* <img className="md:w-35 md:mx-auto md:my-4" src={PieChartImg} /> */}
+            <div className="w-full h-60">
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie
+                            data={pieData}
+                            dataKey="value"
+                            nameKey="name"
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={90}
+                            innerRadius={60}
+                            paddingAngle={3}
+                        >
+                            {pieData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
             <div className="md:mt-2 md:space-y-2">
                 {DATAS.map((data, index) => {
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,48 +10,63 @@ import { FiFileText } from "react-icons/fi";
 import { FiLogIn } from "react-icons/fi";
 
 import LogoImg from "../../assets/logo.png";
+import { useState } from "react";
 
 const NAVLINKS = [
-    { icon: <RxDashboard  size={22} />, title: "Dashboard" },
+    { icon: <RxDashboard size={22} />, title: "Dashboard" },
     { icon: <BiCar size={22} />, title: "Drivers" },
-    { icon: <TbBrandBooking  size={22} />, title: "Bookings" },
-    { icon: <LuBell   size={22} />, title: "Notifications" },
-    { icon: <FiSettings   size={22} />, title: "Settings" },
+    { icon: <TbBrandBooking size={22} />, title: "Bookings" },
+    { icon: <LuBell size={22} />, title: "Notifications" },
+    { icon: <FiSettings size={22} />, title: "Settings" },
 ]
 
 const REPORT_NAVLINKS = [
-    { icon: <MdPayment  size={22} />, title: "Payment Details" },
-    { icon: <AiOutlineTransaction  size={22} />, title: "Transactions" },
-    { icon: <FiFileText  size={22} />, title: "Car Report" },
+    { icon: <MdPayment size={22} />, title: "Payment Details" },
+    { icon: <AiOutlineTransaction size={22} />, title: "Transactions" },
+    { icon: <FiFileText size={22} />, title: "Car Report" },
 ]
 
 export const Navbar = () => {
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const handlerSidebarHoverShow = () => {
+        setShowSidebar(true);
+    }
+
+    const handlerSidebarHoverClose = () => {
+        setShowSidebar(false);
+    }
+
     return (
         <>
-            <nav className="md:w-1/6 md:min-h-screen md:py-6 md:px-4 md:bg-neutral-900 md:flex md:flex-col md:justify-between">
-                <div>
-                    <div className="nav-brand md:w-full">
-                        <img src={LogoImg} className="md:w-fit md:ms-2 md:me-auto md:object-cover" alt="" />
-                    </div>
+            <nav className={` ${showSidebar ? 'md:w-1/6' : 'md:w-0'} md:min-h-screen md:py-6 md:px-4 md:bg-neutral-900 md:flex md:flex-col md:justify-between md:cursor-pointer transition-all duration-200`} onMouseOver={handlerSidebarHoverShow} onMouseLeave={handlerSidebarHoverClose}>
+                {showSidebar &&
+                    <>
+                        <div>
+                            <div className="nav-brand md:w-full">
+                                <img src={LogoImg} className="md:w-fit md:ms-2 md:me-auto md:object-cover" alt="" />
+                            </div>
 
-                    <div className="md:w-full md:flex md:flex-col md:space-y-2 md:text-sm md:text-gray-300  md:py-6 md:border-b md:border-b-gray-600  ">
+                            <div className="md:w-full md:flex md:flex-col md:space-y-2 md:text-sm md:text-gray-300  md:py-6 md:border-b md:border-b-gray-600  ">
 
-                        {NAVLINKS.map((link, index) => <p key={index} className="md:w-full md:flex md:items-center md:gap-3 md:py-3 md:px-4 md:cursor-pointer md:rounded-md hover:bg-gray-800 active:bg-blue-800 active:text-white"><span>{link.icon}</span>{link.title}</p>)}
+                                {NAVLINKS.map((link, index) => <p key={index} className="md:w-full md:flex md:items-center md:gap-3 md:py-3 md:px-4 md:cursor-pointer md:rounded-md hover:bg-gray-800 active:bg-blue-800 active:text-white"><span>{link.icon}</span>{link.title}</p>)}
 
-                    </div>
+                            </div>
 
-                    <div className="md:w-full md:space-y-2 md:py-6">
-                        <p className="md:text-gray-500">Report</p>
+                            <div className="md:w-full md:space-y-2 md:py-6">
+                                <p className="md:text-gray-500">Report</p>
 
-                        <div className="md:w-full md:text-gray-300 md:space-y-2 md:text-sm">
+                                <div className="md:w-full md:text-gray-300 md:space-y-2 md:text-sm">
 
-                            {REPORT_NAVLINKS.map((link, index) => <p key={index} className="md:w-full md:flex md:items-center md:gap-3 md:py-3 md:px-4 md:cursor-pointer md:rounded-md hover:bg-gray-800 active:bg-blue-800 active:text-white"><span>{link.icon}</span>{link.title}</p>)}
+                                    {REPORT_NAVLINKS.map((link, index) => <p key={index} className="md:w-full md:flex md:items-center md:gap-3 md:py-3 md:px-4 md:cursor-pointer md:rounded-md hover:bg-gray-800 active:bg-blue-800 active:text-white"><span>{link.icon}</span>{link.title}</p>)}
 
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <button className="md:text-white md:py-2 md:px-4 md:flex md:items-center md:justify-center md:gap-2 md:rounded-md md:bg-[#464646] md:hover:bg-[#716f6f] md:cursor-pointer"><span><FiLogIn  size={22} /></span>Logout</button>
+                        <button className="md:text-white md:py-2 md:px-4 md:flex md:items-center md:justify-center md:gap-2 md:rounded-md md:bg-[#464646] md:hover:bg-[#716f6f] md:cursor-pointer"><span><FiLogIn size={22} /></span>Logout</button>
+                    </>
+                }
             </nav>
         </>
     )
